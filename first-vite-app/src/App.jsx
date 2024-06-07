bf3740b4feab6b6ce2bc98710e67e71b9d5c490a
Raveesh Samkaria <rishu9417@gmail.com>
2024-06-07 18:36:30 +0530

    Added: Hello World

--- first-vite-app/src/App.jsx
+++ first-vite-app/src/App.jsx
@@ -17,14 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h2>Hello World</h2>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
