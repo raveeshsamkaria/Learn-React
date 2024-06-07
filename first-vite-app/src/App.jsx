414c641269fdcd11f03b963dca732df701cf5afd
Raveesh Samkaria <rishu9417@gmail.com>
2024-06-07 18:45:07 +0530

    Added: Counter using State

--- first-vite-app/src/App.jsx
+++ first-vite-app/src/App.jsx
@@ -1,26 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Counter</h1>
+      <h2>{count}</h2>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>Click Me</button> &nbsp;&nbsp;
+        <button onClick={() => setCount(0)}>Reset</button>
       </div>
-      <h1>Vite + React</h1>
-      <h2>Hello World</h2>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
