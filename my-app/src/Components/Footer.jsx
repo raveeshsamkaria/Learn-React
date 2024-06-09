e548fa1c67e8596b208880acce1208bf67d5ae61
Raveesh Samkaria <rishu9417@gmail.com>
2024-06-09 22:55:10 +0530

    Added: Footer Component

---
+++ my-app/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        Copyright &copy; Raveesh Samkaria || All Rights Reserved
+    </div>
+  )
+}
+
+export default Footer
