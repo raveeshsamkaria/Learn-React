f316aad51d765cc857cf4ac50ab65875650a64eb
Raveesh Samkaria <rishu9417@gmail.com>
2024-06-10 21:22:44 +0530

    Added: Card Component using Props

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,16 @@
-import Footer from './Components/Footer'
 import Navbar from './Components/Navbar'
+import Card from './Components/Card'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="cards">
+        <Card title="Card1" price="$ 123" />
+        <Card title="Card2" price="$ 456" />
+        <Card title="Card3" price="$ 789" />
+      </div>
       <Footer />
     </>
   )
