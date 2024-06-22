3ff6b0405b35ad21eabb44234818c4e8fa64f544
Raveesh Samkaria <rishu9417@gmail.com>
2024-06-22 23:23:26 +0530

    Added: E-Commerce Project with Redux Part-I-Initial Setup

---
+++ e-commerce-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Cart from './Components/Cart'
+import Home from './Components/Home'
+import Navbar from './Components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Cart />
+    </>
+  )
+}
+
+export default App
---
+++ e-commerce-app/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <div>
+            <span>Redux Tutorial</span>
+            <ul>
+                <li>Home</li>
+                <li>Cart</li>
+                <li>Items: 0</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
