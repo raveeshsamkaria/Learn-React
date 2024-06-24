7cd754cb99da9f590aeaf9e6eb6aefb061273682
Raveesh Samkaria <rishu9417@gmail.com>
2024-06-24 22:42:25 +0530

    Added: Product Listing via API

--- e-commerce-app/src/Components/Home.jsx
+++ e-commerce-app/src/Components/Home.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react'
+
 const Home = () => {
+    const [product, setProduct] = useState([])
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const res = await fetch("https://fakestoreapi.com/products")
+            const data = await res.json()
+            setProduct(data)
+        }
+        fetchProduct()
+        }, [])
+
+
     return (
-        <div>Home Content</div>
+        <div className='productWrapper'>
+            {
+                product.map((product) => (
+                    <div className='card' key={product.id}>
+                        <img src={product.image} alt="" />
+                        <h4>{product.title}</h4>
+                        <h5>$ {product.price}</h5>
+                        <button className='btn'>Add to Cart</button>
+                    </div>
+                ))
+            }
+        </div>
     )
 }
 
