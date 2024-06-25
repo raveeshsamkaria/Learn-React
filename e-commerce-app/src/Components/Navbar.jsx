2a919f49e63d64b74525c706ed659c68ecf75caa
Raveesh Samkaria <rishu9417@gmail.com>
2024-06-25 14:48:58 +0530

    Added: E-Commerce Project with Redux Part-II-Setup Redux Store & Slice

--- e-commerce-app/src/Components/Navbar.jsx
+++ e-commerce-app/src/Components/Navbar.jsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+    const items = useSelector((state) => state.cart)
+
     return (
         <div className='nav'>
             <span style={{fontWeight:"bold"}}>Redux Tutorial</span>
             <div>
                 <Link className='navLink' to={'/'}>Home</Link>
                 <Link className='navLink' to={'/cart'}><i className="fa-solid fa-cart-shopping"></i></Link>
-                <span className='navLink'>Items: 0</span>
+                <span className='navLink'>Items: {items.length}</span>
             </div>
         </div>
     )
