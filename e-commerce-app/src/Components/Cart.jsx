6e1867bcfe69448eb8fa11286bd24e357d58bbf3
Raveesh Samkaria <rishu9417@gmail.com>
2024-06-26 23:07:21 +0530

    Added: E-Commerce Project with Redux Part-III-Add to Cart

--- e-commerce-app/src/Components/Cart.jsx
+++ e-commerce-app/src/Components/Cart.jsx
@@ -1,6 +1,20 @@
+import { useSelector } from 'react-redux'
+
 const Cart = () => {
+    const cartItems = useSelector((state) => state.cart)
+
     return (
-        <div>Cart Content</div>
+        <div>
+        {
+            cartItems.map((item) => (
+                <div className='cart' key={item.id}>
+                    <img src={item.image} alt="" />
+                    <h4>{item.title}</h4>
+                    <h5>$ {item.price}</h5>
+                </div>
+            ))
+        }
+        </div>
     )
 }
 
